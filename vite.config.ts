488ef772ae012b8c565e6c3bed9f5cbf5d1f9486
Peter.Anastasov7 <peter.dim.an7@gmail.com>
2025-09-24 17:18:09 +0300

    Mild changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -11,5 +10,10 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
+  },
+  server: {
+    allowedHosts: [
+      'fitness24hub.com',
+    ],
   },
 })
